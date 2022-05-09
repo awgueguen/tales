3ba4363b1987fdf7128d828209981641d07ad6d3
AnicetCelerier <anicet.celerier@gmail.com>
2022-05-09 14:44:03 +0200

    premier merge

--- frontend/src/components/DisplayFriends.jsx
+++ frontend/src/components/DisplayFriends.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { useState } from "react";
-import { useEffect } from "react";
+// imorter tout
 
 
 
 const DisplayFriends = () => {
-  const [request, setRequest] = useState("");
+  const [contacts, setContacts] = useState([]);
+
+  // user/<int:user_id>/contacts
 
   React.useEffect(() => {
-    const apiUrlUsers = "http://localhost:8000/user/1";
+    const apiUrlUsers = "user/1/contacts";
     fetch(apiUrlUsers)
       .then(response => response.json())
-      .then((data) => console.log(data))
+      .then((data) => {console.log(data, 'data')
+        setContacts(data.contacts); })
       .catch(error => console.log(error));
   }, []);
 
-//   const handleChange = (e) => {
-//     setRequest(e.target.value);
-//   };
-//  console.log(MyUserItems)
-//   return (
-// <ul>   
-// {MyUserItems.map((user, i) => (
-//               <li key={i}>
-//                 <img src={user.profile_pic} alt="profile_pic" />
-//                 <p>{user.nickname}</p>
-//                 </li>))}
-// </ul>   
-//     );
-// };
-}
+
+  return (
+<ul>   
+{contacts.map((user, i) => (
+              <li key={i}>
+                <img src={user.profile_pic} alt="profile_pic" />
+                <p>{user.nickname}</p>
+                </li>))}
+</ul>   
+    );
+};
+
 
 export default DisplayFriends;
