0955cdbe0024af3be7fc2e6cb27170d5a208d025
Anx <a.w.gueguen@gmail.com>
2022-05-09 15:39:14 +0200

    pull after merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
-import DisplayFriends from "components/DisplayFriends";
+import DisplayFriends from "@components/DisplayFriends";
 
-import Home from "./pages/Home";
+import Home from "@components/Home";
 import Register from "@pages/Register";
 
 const App = () => {
   return (
     <>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route index path="/displayFriends" element={<DisplayFriends />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route index path="/displayFriends" element={<DisplayFriends />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
