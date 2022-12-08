3495e57ad38ac9cb0cccc933a34069651e0eeb02
SANCHEZ <theo.sanchez@vws.vh20.net>
2022-12-08 16:46:47 +0100

    contact_services : draft

--- frontend/src/services/contacts/contacts.services.js
+++ frontend/src/services/contacts/contacts.services.js
@@ -5,7 +5,7 @@ const API_URL = process.env.REACT_APP_API_ENDPOINT;
 const CONTACTS_URL = {
     plain : `${API_URL}/api/contacts/`,
     add : `${API_URL}/api/contacts/add/`,
-    check : (input) => `${API_URL}/api/room/quick_access?receiver=${input}`
+    check : (input) => `${API_URL}/api/contacts/add/?receiver=${input}`
 };
 
 export const getContacts = async ( token ) => {
@@ -17,11 +17,11 @@ export const getContacts = async ( token ) => {
 export const addContact = async (token, body) => {
 // http://127.0.0.1:8000/api/contacts/add/
     const response = await privateInstance(token).post(CONTACTS_URL.add, body)
-    return response.data
+    return response
 }
 
-export const checkIsFriend = async (token, input) => {
-    // `http://localhost:8000/api/room/quick_access?receiver=${input}`
+export const checkContactExists = async (token, input) => {
+    // `http://localhost:8000/api/contacts/add/?receiver=${input}`
     const response = await privateInstance(token).get(CONTACTS_URL.check(input))
-    return response.data
+    return response
 }
