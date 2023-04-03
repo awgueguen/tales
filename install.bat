d0f76661e1c2fbfcfd83a96f4269d080305f9001
awgueguen <a.w.gueguen@gmail.com>
2023-04-03 16:58:29 +0200

    [FEATURE] - more accurate pip watcher for windows installer

--- install.bat
+++ install.bat
@@ -2,9 +2,13 @@
 @ECHO OFF
 setlocal EnableDelayedExpansion
 
+CLS
 ECHO.
-ECHO -----------------------------------------------------------------
-ECHO [92mTALES V0.1-a[0m 
+ECHO +---------------------------------------------------------------+
+ECHO ^|                           [92mTALES[0m                               ^|
+ECHO +---------------------------------------------------------------+
+ECHO     Version: 1.0a
+ECHO    Released: 11/15/22            
 ECHO -----------------------------------------------------------------
 ECHO.
 
@@ -16,35 +20,44 @@ ECHO [93mIf any issues are encountered, please terminate the script by
 ECHO either using the CTRL-C shortcut or \q in postgres.[0m 
 ECHO No informations are stored whithin this script.
 ECHO.
+:: ^ is the escape character
 ECHO Requirements^: 
 ECHO Python^<3.11, Node^/NPM, Postgres, SASS ^(only for edit^)
 ECHO.
 ECHO -----------------------------------------------------------------
 ECHO.
+:: /p > define an input var
 set /p py=Python usual terminal cmd (e.g. py, python310, python): 
-ECHO.
-ECHO -----------------------------------------------------------------
-ECHO [92mPostgreSQL Installation...[0m 
-ECHO.
-ECHO After connecting to postgres shell use the following cmd:
-ECHO [93m$ postgres=# ^DROP DATABASE dcdb; ^(Optional^)[0m ^> reset dcdb database
-ECHO [93m$ postgres=# ^CREATE DATABASE dcdb; [0m ^> create the app db.
-ECHO [93m$ postgres=# ^\q [0m                    ^> resume the rest of the script.
-ECHO.
 
-:: Avoid to have the user having to start postgres
-set /p input=postgres username: 
-cmd /c "psql -U %input%"
-
-ECHO.
+CLS
 ECHO -----------------------------------------------------------------
+@REM ECHO [92mPostgreSQL Installation...[0m 
+@REM ECHO.
+@REM ECHO After connecting to postgres shell use the following cmd:
+@REM ECHO [93m$ postgres=# ^DROP DATABASE dcdb; ^(Optional^)[0m ^> reset dcdb database
+@REM ECHO [93m$ postgres=# ^CREATE DATABASE dcdb; [0m ^> create the app db.
+@REM ECHO [93m$ postgres=# ^\q [0m                    ^> resume the rest of the script.
+@REM ECHO.
+
+@REM :: Avoid to have the user having to start postgres
+@REM set /p input=postgres username: 
+@REM :: /c > run command and then terminante
+@REM :: /k > run command and then return to the CMD prompt
+@REM cmd /c "psql -U %input%"
+
+@REM CLS
+@REM ECHO -----------------------------------------------------------------
 ECHO [92mVENV ^& PIP Installation...[0m 
 ECHO.
+timeout /t 3 >nul
 
-CD /D backend
+:: cd\ return to root dir
+:: /d > change drive, example, if in C:, you can : CD /D E: 
+CD backend
 
 :: Using start, we can have another shell dealing with all the installation process
-start cmd.exe /c "echo [92mMr.Anderson, welcome back... & %py% -m venv env & env\Scripts\activate & pip install -r requirements.txt"
+:: add splitting later
+start cmd.exe /c "echo [92mMr.Anderson, welcome back... & type NUL > installpipe.txt & %py% -m venv env & echo env.success > installpipe.txt & env\Scripts\activate & pip install -r requirements.txt & echo pip.success > installpipe.txt" 
 for /F %%a in ('copy /Z "%~F0" NUL') do set "CR=%%a"
 
 set /a step=1
@@ -71,37 +84,45 @@ if %step% equ 2 set /P "=-PIP installation                !CR!" < NUL
 goto :monitoring
 
 :end
-:: Buffer to let the process finish completely.
-timeout /t 6 >nul
 
-ECHO.
+timeout /t 3 >nul
+CLS
 ECHO -----------------------------------------------------------------
+:: setup everything in Django
 ECHO [92mDJANGOrest Installation...[0m 
 ECHO.
+timeout /t 3 >nul
 
-CD /D ../../..
 cmd /c "env\Scripts\activate & %py% tales\management\getrandomskey.py"
 ECHO [93mDjango secret key generated in ^.env[0m 
 cmd /c "env\Scripts\activate & %py% manage.py makemigrations tales & %py% manage.py migrate"
+ECHO [93mDjango Migrations complete.[0m 
 
-:: setup everything in Django
-ECHO.
+timeout /t 3 >nul
+CLS
 ECHO -----------------------------------------------------------------
+:: add env creation & createdata
 ECHO [92mTales database population...[0m 
 ECHO.
+timeout /t 3 >nul
 
 cmd /c "env\Scripts\activate & %py% manage.py createdata"
 ECHO [93mDB population done using faker[0m 
 
-:: add env creation & createdata
-ECHO.
+timeout /t 3 >nul
+CLS
 ECHO -----------------------------------------------------------------
 ECHO [92mNPM Installation...[0m 
 ECHO.
+timeout /t 3 >nul
 
 CD /D ../frontend
-cmd /c "npm i"
 
+cmd /c "npm i"
+ECHO [93mNPM dependencies installed.[0m 
+
+timeout /t 3 >nul
+CLS
 ECHO.
 ECHO -----------------------------------------------------------------
 ECHO [93mEverything is now installed, to continue, please start runserver.bat[0m 
@@ -119,25 +140,17 @@ goto :eof
 :: Check at which step the installation of venv & pip is.
 :state
 if %step% equ 1 (
-    CD /D env/Scripts
-    for %%a in (*) do (
-        if %%a equ python.exe (
-            set /a step=2
-            set /P "= [92m-ENV installation COMPLETE                [00m" < NUL
-            ECHO.
-        )
-    )
-    CD /D ../..
+    findstr "env.success" installpipe.txt >nul && (
+        set /a step=2
+        set /P "= [92m-ENV installation COMPLETE                [00m" < NUL
+        ECHO.
+    ) 
 ) else (
-    CD /D env/Lib/site-packages
-    for %%a in (*) do ( 
-        if %%a equ autopep8.py (
-            set /a step=2
-            set /P "= [92m-PIP installation COMPLETE                [00m" < NUL
-            ECHO.
-            goto :end
-        )
+    findstr "pip.success" installpipe.txt >nul && (
+        ECHO env.pip.installation.success > installpipe.txt
+        set /P "= [92m-PIP installation COMPLETE                [00m" < NUL
+        ECHO.
+        goto :end
     )
-    CD /D ../../..
 )
 goto :eof
